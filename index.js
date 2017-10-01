e51558bf429987249e441ecd67a397b3833b15a9
Rishabh Nehra <rishabhnehra@Rishabhs-MacBook-Air.local>
2017-10-01 19:28:01 +0530

    Removed some unnecessary files

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ var express = require("express");
 var app = express();
 app.use("/", express.static(__dirname+"/public"));
 
-app.listen(4000);
+app.listen(4000, function(){
+	console.log("Server running at localhost:4000")
+});
